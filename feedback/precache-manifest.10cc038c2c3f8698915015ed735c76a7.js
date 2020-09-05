e0f122c2e0e1d2f9d2985a7be329c26dc3ab49d3
tkachuks <serhiytkachuk88@gmail.com>
2020-09-05 12:34:11 +0300

    added feedback widget

---
+++ feedback/precache-manifest.10cc038c2c3f8698915015ed735c76a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba210ba274da2a558c531f3cc2894a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78eb1a78e68ebac4b523",
+    "url": "/static/css/main.f22acc6f.chunk.css"
+  },
+  {
+    "revision": "7e46b10de82b91514cb8",
+    "url": "/static/js/2.64d79b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64d79b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78eb1a78e68ebac4b523",
+    "url": "/static/js/main.667f481c.chunk.js"
+  },
+  {
+    "revision": "90870e6884c35fb2ae56",
+    "url": "/static/js/runtime-main.09fdc76c.js"
+  }
+]);
