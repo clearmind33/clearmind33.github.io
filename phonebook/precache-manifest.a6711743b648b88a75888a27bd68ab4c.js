1d0028c07e13ce5188d5c8e1f1fb680b22f3a2dd
tkachuks <serhiytkachuk88@gmail.com>
2020-09-06 21:37:52 +0300

    added phonebook

---
+++ phonebook/precache-manifest.a6711743b648b88a75888a27bd68ab4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2031536ef5eaee38bca14682c91f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175939bf824d5cf10b55",
+    "url": "/static/css/main.1ee590dc.chunk.css"
+  },
+  {
+    "revision": "bea7919bab3cc5adcf7d",
+    "url": "/static/js/2.a0b1cd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0b1cd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175939bf824d5cf10b55",
+    "url": "/static/js/main.c76c312d.chunk.js"
+  },
+  {
+    "revision": "90870e6884c35fb2ae56",
+    "url": "/static/js/runtime-main.09fdc76c.js"
+  }
+]);
