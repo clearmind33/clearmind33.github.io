0f2b1262d1c1b7d46342c9008e92617248dad289
tkachuks <serhiytkachuk88@gmail.com>
2020-09-22 00:06:30 +0300

    upd build

---
+++ phonebook_storage/precache-manifest.8440fa00cea7c41469a186e658caeee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691ea1249b6dc95587a5a78b877d9281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9feeecc1c74397288177",
+    "url": "/static/css/main.1ee590dc.chunk.css"
+  },
+  {
+    "revision": "7ffe7aa9d4dd0bdf6e5e",
+    "url": "/static/js/2.9bf2e431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9bf2e431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9feeecc1c74397288177",
+    "url": "/static/js/main.cac00639.chunk.js"
+  },
+  {
+    "revision": "90870e6884c35fb2ae56",
+    "url": "/static/js/runtime-main.09fdc76c.js"
+  }
+]);
