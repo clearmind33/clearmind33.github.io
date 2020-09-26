31ae4a9bc0a2ee65a870f0dbcb446d11a38ad0b7
tkachuks <serhiytkachuk88@gmail.com>
2020-09-26 11:23:39 +0300

    added image-finder

---
+++ image_finder/precache-manifest.e6edb470c2d275458d7b7dcf951ffac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe83a1364dfe18eaccc445c99d78c3c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4dc0e16f5e148df4ed",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "e943b893f6659300e42d",
+    "url": "/static/css/main.c47d927d.chunk.css"
+  },
+  {
+    "revision": "6f4dc0e16f5e148df4ed",
+    "url": "/static/js/2.29b705f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29b705f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e943b893f6659300e42d",
+    "url": "/static/js/main.10cc8abb.chunk.js"
+  },
+  {
+    "revision": "1430926eb9727524e9ef",
+    "url": "/static/js/runtime-main.5e91ab1a.js"
+  }
+]);
